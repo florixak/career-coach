7ec405ff5ee45766a8d64b968ef2afab56e17ee5
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-02 21:22:07 +0100

    Add error page

---
+++ app/error/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <section className="h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Error</h1>
+      <p>Something went wrong</p>
+    </section>
+  );
+};
+
+export default Page;
