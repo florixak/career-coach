8a858e80a56a2379f0f573b0871f7be4db8cde58
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-02 21:21:19 +0100

    Create header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,22 +2,35 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
+import { createClient } from "@/utils/supabase/server";
 
-const Header = () => {
+const Header = async () => {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
-    <header className="flex z-[100] justify-between items-center py-1 md:py-4 px-5 md:px-10 border-b fixed w-full backdrop-blur-md">
+    <header className="flex z-[100] justify-between items-center py-1 md:py-4 px-5 md:px-10 border-b fixed w-full backdrop-blur-sm">
       <Link href="/">
         <Image
           src="/logo-text.webp"
           alt="Career Coach"
           width={100}
           height={100}
-          className="rounded-full"
         />
       </Link>
 
       <div className="text-lg">
-        <Button variant="outline">Login</Button>
+        {user ? (
+          <Link href="/profile">
+            <Button variant="outline">Profile</Button>
+          </Link>
+        ) : (
+          <Link href="/login">
+            <Button variant="outline">Login</Button>
+          </Link>
+        )}
       </div>
     </header>
   );
