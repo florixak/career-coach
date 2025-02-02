3ef7cca22308e6979a5bcca8b9e0fbe78942ca5f
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-02 10:55:52 +0100

    Create footer

---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-4 text-center bg-muted/50">
+      <div className="container mx-auto text-center">
+        <p>
+          Made with ❤️ by{" "}
+          <Link href="https://github.com/florixak">florixak</Link>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
