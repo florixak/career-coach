14df3ab0b702f3bc2a9816570e68f6b7f16dad42
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-02 10:55:27 +0100

    Edit title

---
+++ components/Title.tsx
@@ -0,0 +1,47 @@
+import VerticalCutReveal from "./fancy/vertical-cut-reveal";
+
+export default function Title() {
+  return (
+    <div className="w-full h-full text-3xl flex flex-col items-start justify-center tracking-wide uppercase font-bold">
+      <VerticalCutReveal
+        splitBy="characters"
+        staggerDuration={0.025}
+        staggerFrom="first"
+        transition={{
+          type: "spring",
+          stiffness: 200,
+          damping: 21,
+        }}
+      >
+        {`HI 👋`}
+      </VerticalCutReveal>
+      <VerticalCutReveal
+        splitBy="characters"
+        staggerDuration={0.025}
+        staggerFrom="last"
+        reverse={true}
+        transition={{
+          type: "spring",
+          stiffness: 200,
+          damping: 21,
+          delay: 0.5,
+        }}
+      >
+        {`Let's build your resume`}
+      </VerticalCutReveal>
+      <VerticalCutReveal
+        splitBy="characters"
+        staggerDuration={0.025}
+        staggerFrom="center"
+        transition={{
+          type: "spring",
+          stiffness: 200,
+          damping: 21,
+          delay: 1.1,
+        }}
+      >
+        {`Together. 🚀`}
+      </VerticalCutReveal>
+    </div>
+  );
+}
