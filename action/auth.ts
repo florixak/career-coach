9d18a2ac67556f1aa423c4fd37e07c17cdcd56ba
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-02 21:20:13 +0100

    Add supabase to project

---
+++ action/auth.ts
@@ -0,0 +1,54 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export const login = async (formData: FormData) => {
+  const supabase = await createClient();
+
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+
+  const { error } = await supabase.auth.signInWithPassword(data);
+
+  if (error) {
+    return {
+      status: "ERROR",
+      error: error.message,
+    };
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/");
+};
+
+export const register = async (formData: FormData) => {
+  const supabase = await createClient();
+
+  const data = {
+    fullName: formData.get("fullName") as string,
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+
+  const { error } = await supabase.auth.signInWithPassword(data);
+
+  if (error) {
+    return error.message;
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/");
+};
+
+export const logout = async () => {
+  const supabase = await createClient();
+
+  await supabase.auth.signOut();
+
+  revalidatePath("/", "layout");
+  redirect("/");
+};
