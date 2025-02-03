a5d42fb8a0324977ce998079f0debddbc5f9ee02
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-03 22:16:42 +0100

    Create resume validation

--- utils/validation.ts
+++ utils/validation.ts
@@ -18,3 +18,23 @@ export const registerFormSchema = z
     message: "Passwords do not match",
     path: ["confirmPassword"],
   });
+
+export const createResumeSchema = z.object({
+  fullName: z
+    .string()
+    .min(3, { message: "Name must be at least 3 characters" }),
+  description: z
+    .string()
+    .min(10, { message: "Description must be at least 10 characters" }),
+  education: z.array(z.string()),
+  workExperience: z.array(z.string()),
+  skills: z.array(z.string()),
+  contact: z.array(z.string()),
+  languages: z.array(z.string()),
+  bgColor: z.string().includes("#", {
+    message: "Background color must be a valid hex color",
+  }),
+  textColor: z.string().includes("#", {
+    message: "Text color must be a valid hex color",
+  }),
+});
