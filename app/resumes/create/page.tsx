cee6c82b0ef6eb728f3a10aa7c27e8707b64e433
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-03 22:20:46 +0100

    Create create resume form

---
+++ app/resumes/create/page.tsx
@@ -0,0 +1,26 @@
+import ResumeCreateForm from "@/components/ResumeCreateForm";
+import { createClient } from "@/utils/supabase/server";
+import { notFound } from "next/navigation";
+
+const Page = async () => {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return notFound();
+  }
+  return (
+    <section className="flex-center flex-col gap-5">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">Create Resume</h1>
+        <p>Fill out the form below to create a new resume</p>
+      </div>
+
+      <ResumeCreateForm />
+    </section>
+  );
+};
+
+export default Page;
---
+++ components/ResumeCreateForm.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+
+const ResumeCreateForm = () => {
+  return (
+    <form className="flex flex-col gap-2 w-full max-w-sm">
+      <div>
+        <Input type="text" name="fullName" placeholder="Full name" />
+      </div>
+      <div>
+        <Textarea
+          name="description"
+          placeholder="Description"
+          className="max-h-[10rem]"
+        />
+      </div>
+
+      <div>
+        <Input name="education" placeholder="Education" />
+      </div>
+
+      <div>
+        <Input name="workExperience" placeholder="Work experience" />
+      </div>
+
+      <div>
+        <Input name="skills" placeholder="Skills" />
+      </div>
+
+      <div>
+        <Input name="contact" placeholder="Contact" />
+      </div>
+
+      <div>
+        <Input name="languages" placeholder="Languages" />
+      </div>
+
+      <div>
+        <Input name="bgColor" placeholder="Background color" />
+      </div>
+
+      <div>
+        <Input name="textColor" placeholder="Text color" />
+      </div>
+    </form>
+  );
+};
+
+export default ResumeCreateForm;
