232149ca0fd98bbfb849d149dce58d4de02e00ad
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-03 22:17:21 +0100

    Add Resume type

---
+++ utils/types.ts
@@ -0,0 +1,28 @@
+export type Resume = {
+  id: string;
+  full_name: string;
+  description: string;
+  education: Education[];
+  experience: Experience[];
+  skills: string[];
+  contact: string[];
+  languages: string[];
+  image_url: string;
+  created_at: string;
+  updated_at: string;
+};
+
+type Education = {
+  degree: string;
+  school: string;
+  end_date: string;
+  start_date: string;
+};
+
+type Experience = {
+  company: string;
+  position: string;
+  description: string;
+  end_date: string;
+  start_date: string;
+};
