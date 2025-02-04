2b63bc072d14dc3a05c983339b9801089b699219
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-04 22:11:21 +0100

    Edit logout button logic

--- components/ButtonLogout.tsx
+++ components/ButtonLogout.tsx
@@ -2,12 +2,33 @@
 
 import { logout } from "@/action/auth";
 import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const ButtonLogout = () => {
+  const router = useRouter();
+
   const handleLogout = async () => {
-    await logout();
+    await logout()
+      .then(() => {
+        toast.success("Logged out successfully");
+        router.push("/login");
+      })
+      .catch(() => {
+        toast.error("An error occurred while logging out");
+      });
   };
-  return <Button onClick={handleLogout}>Logout</Button>;
+  return (
+    <Button
+      onClick={handleLogout}
+      variant="outline"
+      className="flex items-center"
+    >
+      <span className="hidden sm:block">Logout</span>
+      <LogOut />
+    </Button>
+  );
 };
 
 export default ButtonLogout;
