0f92f273453a9cbe9bffbc452e444c70b73e7ebf
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-04 22:12:45 +0100

    Add menu bar for mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,13 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
 import { createClient } from "@/utils/supabase/server";
-import { Newspaper, User } from "lucide-react";
+import { Home, Menu, Newspaper, User } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@radix-ui/react-dropdown-menu";
 
 const Header = async () => {
   const supabase = await createClient();
@@ -23,28 +29,62 @@ const Header = async () => {
       </Link>
       <nav className="flex gap-5">
         {user ? (
-          <Link href="/resumes">
+          <Link href="/resumes" className="hidden sm:block">
             <Button variant="outline">
               <span className="hidden sm:block">Resumes</span>
               <Newspaper />
             </Button>
           </Link>
         ) : null}
+
         {user ? (
-          <Link href="/profile">
+          <Link href="/profile" className="hidden sm:block">
             <Button variant="outline" className="sm:w-auto">
-              <span className="hidden sm:block">Profile</span>
+              <span className="hidden sm:block">
+                {user?.email || "Profile"}
+              </span>
               <User />
             </Button>
           </Link>
         ) : (
-          <Link href="/login">
+          <Link href="/login" className="hidden sm:block">
             <Button variant="outline" className="sm:w-auto">
               <span className="hidden sm:block">Login</span>
               <User />
             </Button>
           </Link>
         )}
+
+        <div className="block sm:hidden text-lg z-[110]">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Menu size={54} />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="rounded-md p-2 space-y-3 bg-background w-[10rem]">
+              <DropdownMenuItem>
+                <Link href="/" className="flex gap-2">
+                  Home <Home />
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link
+                  href={user ? "/profile" : "/login"}
+                  className="flex gap-2"
+                >
+                  {user ? "Profile" : "Login"} <User />
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link
+                  href={user ? "/resumes" : "/login"}
+                  className="flex gap-2"
+                >
+                  Resumes <Newspaper />
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </nav>
     </header>
   );
