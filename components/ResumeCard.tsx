34105f69e7bb0e67962793b3b58795bcf043229d
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-04 15:26:05 +0100

    Add clickable resume card

--- components/ResumeCard.tsx
+++ components/ResumeCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Resume from "./Resume";
+import Link from "next/link";
 
 type ResumeCardProps = {
   resume: Resume;
@@ -14,14 +15,16 @@ const ResumeCard = ({ resume }: ResumeCardProps) => {
   });
 
   return (
-    <article className="relative border p-4 rounded-md w-full md:w-[26rem] h-[20rem] object-fill overflow-hidden cursor-pointer">
-      <div className="absolute w-full h-full hover:bg-black hover:bg-opacity-50 flex transition-all duration-500 items-center justify-center group top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <p className="text-white hidden group-hover:flex transition-all">
-          {formatDay}
-        </p>
-      </div>
-      <Resume resumeId={resume.id} />
-    </article>
+    <Link href={`/resume/${resume.id}`}>
+      <article className="relative border p-4 rounded-md w-full md:w-[26rem] h-[20rem] object-fill overflow-hidden cursor-pointer">
+        <div className="absolute w-full h-full hover:bg-black hover:bg-opacity-50 flex transition-all duration-500 items-center justify-center group top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <p className="text-white hidden group-hover:flex transition-all">
+            {formatDay}
+          </p>
+        </div>
+        <Resume resumeId={resume.id} />
+      </article>
+    </Link>
   );
 };
 
