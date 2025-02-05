8156fc5a89d99b5d644b3f721376fdc731a1679f
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-05 13:35:52 +0100

    Add await params

--- app/(resume)/resume/[id]/page.tsx
+++ app/(resume)/resume/[id]/page.tsx
@@ -2,9 +2,9 @@ import { getResume } from "@/action/resume";
 import { redirect } from "next/navigation";
 import React from "react";
 
-const Page = async ({ params }: { params: { id: string } }) => {
-  const { id } = params;
-  const resume = await getResume(id);
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+  const resume = await getResume(decodeURI(id));
 
   if (resume.status === "ERROR") {
     redirect("/login");
