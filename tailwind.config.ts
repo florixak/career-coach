5cde0118c9c4e61a6c0b58b3b7cd0993e75ae005
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-09 22:12:44 +0100

    Add animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,10 +73,16 @@ export default {
             height: "0",
           },
         },
+        shine: {
+          "0%": { backgroundPosition: "200% 0" },
+          "25%": { backgroundPosition: "-200% 0" },
+          "100%": { backgroundPosition: "-200% 0" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        shine: "shine 3s ease-out infinite",
       },
       fontFamily: {
         "work-sans": ["var(--font-work-sans)"],
---
+++ utils/animations.ts
@@ -0,0 +1,14 @@
+export const scaleUp = {
+  initial: {
+    scale: 0.7,
+  },
+  whileInView: {
+    scale: 1,
+  },
+  whileHover: {
+    scale: 1.1,
+  },
+  exit: {
+    scale: 0.7,
+  },
+};
