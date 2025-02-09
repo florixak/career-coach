ed6f2b12bd8ab662991f1cf1f75bf48ce07ef229
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-09 22:15:58 +0100

    Edit landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import GetStartedButton from "@/components/GetStartedButton";
+import ReviewSection from "@/components/reviews/ReviewSection";
 import Title from "@/components/Title";
-import { Button } from "@/components/ui/button";
+import ErrorBoundary from "next/dist/client/components/error-boundary";
 import Image from "next/image";
-import Link from "next/link";
+import Error from "./resumes/error";
 
 export default function Home() {
   return (
@@ -17,17 +19,20 @@ export default function Home() {
         />
         <article className="flex flex-col space-y-10 items-start">
           <Title />
-          <Button variant="outline" className="text-xl p-5 font-normal">
-            <Link href="/resumes">Get Started</Link>
-          </Button>
+
+          <GetStartedButton />
         </article>
       </section>
-      <section className="flex flex-col items-center justify-center h-screen bg-muted/50 px-10">
-        <h2>
-          Career Coach is a resume builder that helps you create a professional
-          resume in minutes.
-        </h2>
+      <section className="flex-center flex-col bg-muted/50 px-10 gap-5">
+        <h2 className="text-2xl font-bold">About Career Coach</h2>
+        <p className="text-lg max-w-[40rem]">
+          Career Coach is a platform to create professional resumes. You can
+          choose from multiple templates and download your resume in PDF format.
+          You can also share your resume with a link.
+        </p>
       </section>
+
+      <ReviewSection />
     </>
   );
 }
