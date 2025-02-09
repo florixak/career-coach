9b350421b53bb235d75fca066fe07ab2b000d1be
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-09 22:15:02 +0100

    Add dynamic page for resume

---
+++ app/resumes/[id]/page.tsx
@@ -0,0 +1,20 @@
+import { getResume } from "@/action/resume";
+import { redirect } from "next/navigation";
+import React from "react";
+
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+  const resume = await getResume(decodeURIComponent(id));
+
+  if (resume.status === "ERROR") {
+    redirect("/login");
+  }
+
+  return (
+    <div className="flex-center text-white">
+      {resume ? resume?.resume?.full_name : "No resume found"}
+    </div>
+  );
+};
+
+export default Page;
