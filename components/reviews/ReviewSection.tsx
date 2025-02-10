c91cd840ff347d9d98faf47d6100b0b953ad991f
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-10 22:12:05 +0100

    Create method for slice

--- components/reviews/ReviewSection.tsx
+++ components/reviews/ReviewSection.tsx
@@ -6,11 +6,19 @@ import Link from "next/link";
 
 const ReviewSection = async () => {
   const { reviews } = await getReviews();
+
+  const spliceReviews = (end: number) => reviews?.slice(0, end);
+
   return (
     <section className="flex justify-center flex-col items-center px-10 gap-5 md:h-screen my-10">
       <h2 className="text-2xl font-bold">Reviews</h2>
-      <ul className="flex gap-5 flex-wrap max-w-[50rem]">
-        {reviews?.splice(0, 8)?.map((review) => {
+      <ul className="md:flex gap-5 flex-wrap max-w-[50rem] hidden">
+        {spliceReviews(8)?.map((review) => {
+          return <ReviewCard key={review.id} review={review} />;
+        })}
+      </ul>
+      <ul className="md:hidden flex flex-col gap-5">
+        {spliceReviews(4)?.map((review) => {
           return <ReviewCard key={review.id} review={review} />;
         })}
       </ul>
