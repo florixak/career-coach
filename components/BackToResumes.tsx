3f279fa7412b30373f04ea25ef925fe03d560cc0
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-10 22:12:51 +0100

    Create back to resumes

---
+++ components/BackToResumes.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const BackToResumes = () => {
+  return (
+    <Button variant="outline" className="absolute -top-3 left-5">
+      <Link href="/resumes">Back to resumes</Link>
+    </Button>
+  );
+};
+
+export default BackToResumes;
