971820aceaf70fa8bec466d81cf49bab590c058c
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-12 22:00:14 +0100

    Move user to navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { createClient } from "@/utils/supabase/server";
 import Navbar from "./Navbar";
 
 const Header = async () => {
-  const supabase = await createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
   return (
     <header className="flex z-[100] justify-between items-center py-1 md:py-2 px-5 md:px-10 border-b fixed w-full backdrop-blur-sm transition-all duration-1000">
       <Link href="/">
@@ -20,7 +14,7 @@ const Header = async () => {
           height={128}
         />
       </Link>
-      <Navbar user={user} />
+      <Navbar />
     </header>
   );
 };
