12aca5e42ab5ba741f9dce629c1f776d09c4b14f
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-12 16:48:16 +0100

    Create reviews page

--- app/(root)/reviews/page.tsx
+++ app/(root)/reviews/page.tsx
@@ -1,5 +1,19 @@
-const Page = () => {
-  return <div>page</div>;
+import { getReviews } from "@/action/reviews";
+import ReviewCard from "@/components/reviews/ReviewCard";
+
+const Page = async () => {
+  const { reviews } = await getReviews();
+
+  return (
+    <section className="flex items-center justify-center flex-col gap-5 text-white mt-48 mb-20">
+      <h1>Reviews</h1>
+      <div className="flex flex-wrap gap-5 max-w-[60rem]">
+        {reviews?.map((review) => (
+          <ReviewCard key={review.id} review={review} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Page;
