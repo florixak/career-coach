32305e4fbeb68ec8e867908dd56949d6de8230f4
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-17 10:01:38 +0100

    Move loading to root route folder

---
+++ app/(root)/loading.tsx
@@ -0,0 +1,7 @@
+import LoadingSticks from "@/components/LoadingSticks";
+
+const Loading = () => {
+  return <LoadingSticks className="flex-center" />;
+};
+
+export default Loading;
--- app/loading.tsx
+++
@@ -1,7 +0,0 @@
-import { TextShimmer } from "@/components/ui/text-shimmer";
-
-const Loading = () => {
-  return <TextShimmer>Loading</TextShimmer>;
-};
-
-export default Loading;
