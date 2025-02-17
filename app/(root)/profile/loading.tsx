3511c328d475a222e61543fbc50c876b0376f54f
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-17 10:01:27 +0100

    Remove redundant loading

--- app/(root)/profile/loading.tsx
+++
@@ -1,7 +0,0 @@
-import { TextShimmer } from "@/components/ui/text-shimmer";
-
-const Loading = () => {
-  return <TextShimmer className="text-5xl font-bold">Loading</TextShimmer>;
-};
-
-export default Loading;
