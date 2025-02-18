3ce08b8d6367a35d10ed06b763e2fe7e75f9d170
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-18 16:06:59 +0100

    Edit styles and names

--- components/forms/ResumeCreateForm.tsx
+++ components/forms/ResumeCreateForm.tsx
@@ -5,22 +5,26 @@ import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Textarea } from "../ui/textarea";
 import { useRef } from "react";
+import { Wand } from "lucide-react";
+import toast from "react-hot-toast";
 
 const ResumeCreateForm = () => {
   const descriptionRef = useRef<HTMLTextAreaElement>(null);
 
-  const handleEditWithAI = async () => {
-    const prompt = descriptionRef.current?.value || "";
+  const handleImproveWithAI = async () => {
+    const prompt = descriptionRef.current?.value || null;
     const response = await improveDescriptionWithAI(prompt);
     if (response.status === "SUCCESS") {
       if (descriptionRef.current) {
         descriptionRef.current.value = response.description || "";
       }
     }
+
+    toast[response.status === "SUCCESS" ? "success" : "error"](response.error);
   };
 
   return (
-    <form className="flex flex-col gap-2 w-full max-w-sm">
+    <form className="flex flex-col gap-2 max-w-sm">
       <div>
         <Input type="file" name="image" accept="image/*" placeholder="Image" />
       </div>
@@ -28,15 +32,23 @@ const ResumeCreateForm = () => {
       <div>
         <Input type="text" name="fullName" placeholder="Full name" />
       </div>
-      <div>
+      <div className="flex flex-row gap-2 w-full">
         <Textarea
           name="description"
           placeholder="Description"
           className="max-h-[10rem]"
           ref={descriptionRef}
         />
-        <Button type="button" variant="outline" onClick={handleEditWithAI}>
-          Edit with AI
+        <Button
+          type="button"
+          variant="outline"
+          onClick={handleImproveWithAI}
+          className="relative group"
+        >
+          <span className="absolute left-12 top-3 bottom-0 items-center pl-2 text-muted-foreground opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+            Improve with AI
+          </span>
+          <Wand size={16} />
         </Button>
       </div>
 
