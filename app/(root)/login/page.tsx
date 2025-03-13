6306a1365c0869b0b72359ba0d6bd015c6516de1
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-13 17:02:13 +0100

    Remove space in styles

--- app/(root)/login/page.tsx
+++ app/(root)/login/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
     <section className="flex-center flex-col gap-5">
       <h1 className="text-4xl font-bold">Login</h1>
       <LoginForm />
-      <div className=" w-1/5 border-t rounded-full text-center"></div>
+      <div className="w-1/5 border-t rounded-full text-center"></div>
       <p>
         {"Don't have an account? "}
         <Link href="/register" className="text-blue-400">
