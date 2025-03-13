ed74fb8289439cc02e841e2e6012b9429936c20d
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-13 18:31:09 +0100

    Remove undefined type

--- components/resumes/ResumesList.tsx
+++ components/resumes/ResumesList.tsx
@@ -2,7 +2,7 @@ import { type Resume } from "@/utils/types";
 import ResumeCard from "./ResumeCard";
 
 type ResumesListProps = {
-  resumes: Resume[] | undefined;
+  resumes: Resume[];
 };
 
 const ResumesList = ({ resumes }: ResumesListProps) => {
